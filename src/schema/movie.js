1bf0344379a359c122ac7217b895cee0078c6a9c
ErnstWevers <ernstwevers@gmail.com>
2018-01-25 16:19:37 +0100

    set the local env for the API KEY

--- src/schema/movie.js
+++ src/schema/movie.js
@@ -1,7 +1,7 @@
 import { makeExecutableSchema } from 'graphql-tools';
 import http from 'request-promise-json';
 
-const MOVIE_DB_API_KEY = process.env.MOVIE_DB_API_KEY;
+const MOVIEDBAPIKEY = process.env.MOVIEDBAPIKEY;
 const OMDB_API_KEY = process.env.OMDB_API_KEY;
 
 const typeDefs = `
@@ -31,29 +31,29 @@ const resolvers = {
     movie: async (obj, args, context, info) => {
       if (args.id) {
         return http
-          .get(`https://api.themoviedb.org/3/movie/${args.id}?api_key=${MOVIE_DB_API_KEY}&language=en-US`)
+          .get(`https://api.themoviedb.org/3/movie/${args.id}?api_key=${MOVIEDBAPIKEY}&language=en-US`)
       }
       if (args.imdb_id) {
         const results = await http
-          .get(`https://api.themoviedb.org/3/find/${args.imdb_id}?api_key=${MOVIE_DB_API_KEY}&language=en-US&external_source=imdb_id`)
+          .get(`https://api.themoviedb.org/3/find/${args.imdb_id}?api_key=${MOVIEDBAPIKEY}&language=en-US&external_source=imdb_id`)
 
         if (results.movie_results.length > 0) {
           const movieId = results.movie_results[0].id
           return http
-            .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${MOVIE_DB_API_KEY}&language=en-US`)
+            .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${MOVIEDBAPIKEY}&language=en-US`)
         }
       }
     },
     person: async (obj, args, context, info) => {
       if(args.id){
         return http
-          .get(`https://api.themoviedb.org/3/person/${args.id}?api_key=${MOVIE_DB_API_KEY}&language=en-US`)
+          .get(`https://api.themoviedb.org/3/person/${args.id}?api_key=${MOVIEDBAPIKEY}&language=en-US`)
       }
     },
     movies: async (obj, args, context, info) => {
       if(args.title) {
         const response = await http
-          .get(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_DB_API_KEY}&language=en-US&page=1&include_adult=false&query=${args.title}`)
+          .get(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIEDBAPIKEY}&language=en-US&page=1&include_adult=false&query=${args.title}`)
         return response.results
       }
     }
