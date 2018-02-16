3c2b916d151978a85b3376e833c63560564f6b46
ErnstWevers <ernstwevers@gmail.com>
2018-02-16 16:27:02 +0100

    add schema for cast/crew

--- src/schema/movie.js
+++ src/schema/movie.js
@@ -15,15 +15,22 @@ const typeDefs = `
     runtime: String
   }
 
-  type Person {
-    name: String
-  }
-
   type Query {
     movies(title: String): [Movie]
-    person(id: String): Person
     movie(id: String, imdb_id: String): Movie
+    cast(id:String): Movie
     test : String
+  }
+
+  type Cast {
+    id: ID
+    cast: [Person]
+    crew: [Person]
+  }
+
+  type Person {
+    id: ID
+    name: String
   }
 `;
 
@@ -41,10 +48,12 @@ const resolvers = {
           .get(`https://api.themoviedb.org/3/find/${args.imdb_id}?api_key=${MOVIEDBAPIKEY}&language=en-US&external_source=imdb_id`)
       }
     },
-    person: async (obj, args, context, info) => {
+    cast: async (obj, args, context, info) => {
       if(args.id){
-        return http
-          .get(`https://api.themoviedb.org/3/person/${args.id}?api_key=${MOVIEDBAPIKEY}&language=en-US`)
+        console.log(`getting cast for movie with id: ${args.id}`);
+        const response = await http
+          .get(`https://api.themoviedb.org/3/movie/${args.id}?api_key=${MOVIEDBAPIKEY}&language=en-US`)
+          console.log(response);
       }
     },
     movies: async (obj, args, context, info) => {
@@ -53,6 +62,13 @@ const resolvers = {
         const response = await http
           .get(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIEDBAPIKEY}&language=en-US&page=1&include_adult=false&query=${args.title}`)
         return response.results
+      }
+    },
+    cast: async (obj, args, context, info) => {
+      if(args.id){
+        const response = await http
+          .get(`https://api.themoviedb.org/3/movie/${args.id}?api_key=${MOVIEDBAPIKEY}&language=en-US`)
+        return response
       }
     },
     //approach this API endpointURL with a post the correct header
