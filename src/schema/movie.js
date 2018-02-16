81d74391c17da456feb69e856fcadb2aabe10989
ErnstWevers <ernstwevers@gmail.com>
2018-02-16 15:34:32 +0100

    added logs for requests

--- src/schema/movie.js
+++ src/schema/movie.js
@@ -31,18 +31,14 @@ const resolvers = {
   Query: {
     movie: async (obj, args, context, info) => {
       if (args.id) {
+        console.log(`fetching movie with id: ${args.id}`)
         return http
           .get(`https://api.themoviedb.org/3/movie/${args.id}?api_key=${MOVIEDBAPIKEY}&language=en-US`)
       }
       if (args.imdb_id) {
+        console.log(`fetching movie with imdb_id: ${args.imdb_id}`)
         const results = await http
           .get(`https://api.themoviedb.org/3/find/${args.imdb_id}?api_key=${MOVIEDBAPIKEY}&language=en-US&external_source=imdb_id`)
-
-        if (results.movie_results.length > 0) {
-          const movieId = results.movie_results[0].id
-          return http
-            .get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${MOVIEDBAPIKEY}&language=en-US`)
-        }
       }
     },
     person: async (obj, args, context, info) => {
@@ -53,6 +49,7 @@ const resolvers = {
     },
     movies: async (obj, args, context, info) => {
       if(args.title) {
+        console.log(`searching for "${args.title}"`)
         const response = await http
           .get(`https://api.themoviedb.org/3/search/movie?api_key=${MOVIEDBAPIKEY}&language=en-US&page=1&include_adult=false&query=${args.title}`)
         return response.results
